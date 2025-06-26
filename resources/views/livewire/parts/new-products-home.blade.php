5e992ef10129551ee484f805e29b901424704692
Omar Abi Farraj <omar.7tech@gmail.com>
2025-06-27 02:52:41 +0300

    livewire Tags fix

--- resources/views/livewire/parts/new-products-home.blade.php
+++ resources/views/livewire/parts/new-products-home.blade.php
@@ -1,22 +1,24 @@
-@if ($products->isNotEmpty())
-    <section class=" mx-auto px-4 py-8 bg-base-200 animate__animated animate__fadeIn">
-        <div class="flex justify-between items-center mb-6  bg-base-200  pb-4">
-            <h2 class="text-2xl sm:text-3xl font-bold">
-                New Products
-            </h2>
-            <a wire:navigate href="{{ route('products', ['activeTab=new']) }}"
-                class="btn btn-link hover:scale-105 transition-transform text-sm sm:text-base">View All →</a>
-        </div>
-
-        <div class="relative">
-            <div class="flex overflow-x-auto pb-6 -mx-4 px-4 scrollbar-hide gap-3">
-                @foreach ($products as $product)
-                    <div wire:key="{{ $product->slug }}"
-                        class="flex-none w-[45%] sm:w-[30%] md:w-[25%] lg:w-[15%] min-w-[180px]">
-                        <livewire:product-card :$product :key="$product->slug" />
-                    </div>
-                @endforeach
+<div>
+    @if ($products->isNotEmpty())
+        <section class=" mx-auto px-4 py-8 bg-base-200 animate__animated animate__fadeIn">
+            <div class="flex justify-between items-center mb-6  bg-base-200  pb-4">
+                <h2 class="text-2xl sm:text-3xl font-bold">
+                    New Products
+                </h2>
+                <a wire:navigate href="{{ route('products', ['activeTab=new']) }}"
+                    class="btn btn-link hover:scale-105 transition-transform text-sm sm:text-base">View All →</a>
             </div>
-        </div>
-    </section>
-@endif
+
+            <div class="relative">
+                <div class="flex overflow-x-auto pb-6 -mx-4 px-4 scrollbar-hide gap-3">
+                    @foreach ($products as $product)
+                        <div wire:key="{{ $product->slug }}"
+                            class="flex-none w-[45%] sm:w-[30%] md:w-[25%] lg:w-[15%] min-w-[180px]">
+                            <livewire:product-card :$product :key="$product->slug" />
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+    @endif
+</div>
